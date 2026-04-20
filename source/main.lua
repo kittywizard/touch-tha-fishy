182107adf74408c83d394bebd25a503b3c7ade7b
kittywizard <volcana@gmail.com>
2026-04-20 16:17:09 -0700

    tutorial basics

--- source/main.lua
+++ source/main.lua
@@ -1,22 +1,40 @@
+import "CoreLibs/graphics"
+import "CoreLibs/sprites"
+
 local pd = playdate
 local gfx = pd.graphics
 
 -- player example of the axis: x = 400/ y = 240 (bottom left corner)
-local playerX = 40
-local playerY = 120
+local playerStartX = 40
+local playerStartY = 120
 local playerSpeed = 3
 local playerImage = gfx.image.new("img/sprite")
+local playerSprite = gfx.sprite.new(playerImage)
+playerSprite:setCollideRect(0, 0, 32, 32) --x, y relative to sprite, size of sprite in px
+playerSprite:moveTo(playerStartX, playerStartY)
+playerSprite:add()
+
+-- game state
+local gameState = "stopped"
 
 function pd.update()
     -- 30 FPS
-    gfx.clear()
-
-    local crankPosition = pd.getCrankPosition()
-    if crankPosition <= 90 or crankPosition >= 270 then
-        playerY -= playerSpeed
-    else
-        playerY += playerSpeed
-    end 
-
-    playerImage:draw(playerX, playerY)
+    gfx.sprite.update()
+    if gameState == "stopped" then
+        gfx.drawTextAligned("press A to start", 200, 40, kTextAlignment.center)
+        if pd.buttonJustPressed(pd.kButtonA) then
+            gameState = "active"
+            playerSprite:moveTo(playerStartX, playerStartY)
+        end
+    elseif gameState == "active" then
+        local crankPosition = pd.getCrankPosition()
+        if crankPosition <= 90 or crankPosition >= 270 then
+            playerSprite:moveBy(0, -playerSpeed)
+        else
+            playerSprite:moveBy(0, playerSpeed)
+        end 
+        if playerSprite.y > 270 or playerSprite.y < -30 then
+            gameState = "stopped"
+        end
+    end
 end
