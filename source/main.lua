3b4d74d2422729e8bd7dbdbd4df18e651373ec42
kittywizard <volcana@gmail.com>
2026-04-22 10:11:51 -0700

    tweaks towards game play

--- source/main.lua
+++ source/main.lua
@@ -4,14 +4,15 @@ import "CoreLibs/sprites"
 local pd = playdate
 local gfx = pd.graphics
 
+local splashscreen = gfx.image.new("img/splashscreen2")
 -- player example of the axis: x = 400/ y = 240 (bottom left corner)
 local playerStartX = 40
 local playerStartY = 230
-local playerSpeed = 3
+local playerSpeed = 1
 local playerImage = gfx.image.new("img/paw")
 local playerSprite = gfx.sprite.new(playerImage)
-local splashscreen = gfx.image.new("img/splashscreen2")
-playerSprite:setCollideRect(0, 50, 20, 50) --x, y relative to sprite, size of sprite in px
+
+playerSprite:setCollideRect(0, 0, 20, 50) --x, y relative to sprite, size of sprite in px
 playerSprite:moveTo(playerStartX, playerStartY)
 playerSprite:add()
 
@@ -20,20 +21,20 @@ local gameState = "stopped"
 local score = 0
 
 -- obstacle
-local obstacleSpeed = 5
+local obstacleSpeed = 0
 local obstacleImage = gfx.image.new("img/fish")
 local obstacleSprite = gfx.sprite.new(obstacleImage)
 obstacleSprite.collisionResponse = gfx.sprite.kCollisionTypeOverlap
-obstacleSprite:setCollideRect(0, 0, 64, 64)
+obstacleSprite:setCollideRect(0, 0, 57, 57)
 obstacleSprite:moveTo(200, 20)
 obstacleSprite:add()
 
 function pd.update()
     -- 30 FPS
-    gfx.sprite.update()
+    gfx.sprite:update()
     if gameState == "stopped" then
         splashscreen:draw(0, 0)
-        gfx.drawTextAligned("press A to start", 200, 40, kTextAlignment.center)
+        --gfx.drawTextAligned("press A to start", 200, 40, kTextAlignment.center)
 
         if pd.buttonJustPressed(pd.kButtonA) then
             gameState = "active"
@@ -47,8 +48,10 @@ function pd.update()
         local crankPosition = pd.getCrankPosition()
         if crankPosition <= 90 or crankPosition >= 270 then
             playerSprite:moveBy(0, -playerSpeed)
-        else
+        elseif crankPosition > 90 or crankPosition < 270 then
             playerSprite:moveBy(0, playerSpeed)
+        else    
+            playerSprite:moveBy(0,0)
         end 
 
         if pd.buttonJustPressed(pd.kButtonLeft) then
@@ -59,11 +62,16 @@ function pd.update()
 
         local actualX, actualY, collisions, length = obstacleSprite:moveWithCollisions(obstacleSprite.x - obstacleSpeed, obstacleSprite.y)
         -- complex games will need to check more than just length > 0 
-        if obstacleSprite.x < -20 then
-            obstacleSprite:moveTo(20, math.random(40, 200))
+        -- if obstacleSprite.x < -20 then
+        --     obstacleSprite:moveTo(20, math.random(40, 200))
+        --     score += 1
+        -- end
+        if length > 0 then
             score += 1
+            -- gameState = "stopped"
+            obstacleSprite:moveTo(20, math.random(40, 200))
         end
-        if length > 0 or playerSprite.y > 270 or playerSprite.y < -30 then
+        if playerSprite.y > 270 or playerSprite.y < -30  then
             gameState = "stopped"
         end
     end
